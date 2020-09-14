717bf9a6401cd71a1a044bde649b82d8bd9e1757
amdevito <amdevitoam@gmail.com>
2020-09-14 18:16:04 -0400

    experimenting with colour and shapes

--- Experiments/drawing experiments/js/script.js
+++ Experiments/drawing experiments/js/script.js
@@ -10,16 +10,23 @@ Here is a description of this template p5 project.
 // Description of setup() goes here.
 function setup() {
   createCanvas(800, 800); // set canvas size (width, height)
-    background(200, 123, 200);
+    background(200, 123, 200, 170); // RBGA < a for alpha 0 = completely transparent, 250 = solid)
+    stroke(250, 255, 0); // good palette! lilac, turquoise, yellow//
 rectMode(CORNER);
+
+fill(0, 160, 190, 50);
   rect(400, 400, 100, 100);
   rect(400, 400, 80, 80, 0, 0, 20, 0);
+  fill(0, 190, 190, 50);
   rect(400, 400, 60, 60);
   rect(400, 400, 40, 40, 0, 0, 20, 0);
-  rect(400, 400, 20, 20);
+  ellipseMode(CORNER);
+  noStroke(); // removes outline from everything at follows
   ellipse(200, 400, 100, 100);
-  ellipse(200, 400, 60, 60);
-  ellipse(200, 400, 20, 20);
+  fill(0, 165, 190, 100);
+  ellipse(200, 390, 60, 60);
+  fill(0, 170, 200, 100);
+  ellipse(200, 380, 40, 40);
 
 
 
