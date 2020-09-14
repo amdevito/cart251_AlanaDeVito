2d3edb31effd4fe2fa61fa287b3097ad46a6425b
amdevito <amdevitoam@gmail.com>
2020-09-14 16:22:35 -0400

    Added Experiments folder

---
+++ Experiments/template-p5-project/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(25, 300, 20); // tells p5 to make the background RED 244 = red
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+}
