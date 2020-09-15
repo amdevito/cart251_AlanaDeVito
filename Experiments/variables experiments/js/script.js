1f56a76cb716d01c89fafeb9a21c52a7f270e6a5
amdevito <amdevitoam@gmail.com>
2020-09-15 16:19:07 -0400

    interactive, size and colour changing rectangles

--- Experiments/variables experiments/js/script.js
+++ Experiments/variables experiments/js/script.js
@@ -1,20 +1,28 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Variables Experiment
+Alana DeVito
 
-Here is a description of this template p5 project.
+As the cursor moves around the black canvas (which is the adaptable size of the browser window),
+the rectangle changes size and shape along with changing between different shades of purple, green and grey. Everything
+has a tail that remains because the background draw function is removed. I would like to reuse this for something. Really love the reactivity and look.
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
 
 }
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {
+function draw() { /// writing 1/60th a second 'draw loop' or 'update loop'
+//background(mouseX, mouseY, 0);
+  rectMode(CENTER);
+  fill(mouseY / 3, mouseX / 2, mouseY / 3);
+  rect(width / 2, height / 2, mouseX / 2, mouseY / 2);//(location determined by mouse, and size determined by mouse)
 
 }
