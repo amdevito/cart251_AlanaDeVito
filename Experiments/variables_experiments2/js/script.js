c197f89ec36350d2a9505a3a4135ee44dba159dd
amdevito <amdevitoam@gmail.com>
2020-09-15 18:25:40 -0400

    created green and black tunnel animation
    learning about changing variables for the purpose of movement and animation

--- Experiments/variables_experiments2/js/script.js
+++ Experiments/variables_experiments2/js/script.js
@@ -6,7 +6,9 @@ Alana DeVito
 let backgroundShade = 0;
 let circleX = 250;
 let circleY = 250;
-let circleSize = 200; //state variable before set up
+let circleSize = 400; //state variable before set up
+let circleSpeed = -3.8; // have the negative in the variable not in the arguement
+let circleAcceleration = 0.25;
 // setup()
 //
 // Description of setup() goes here.
@@ -20,6 +22,13 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() { /// writing 1/60th a second 'draw loop' or 'update loop'
-background(backgroundShade);
+//background(backgroundShade + 2); //activate these and you lose the ripple effect but get the backgroun changing from black to white and the green circle shoots off into the distance
+//backgroundShade = backgroundShade + 2;
+strokeWeight(3);
+stroke(0, 255, 0);
+circleSize = circleSize * 0.93; // keeps moving because program keeps looping around to it
+circleY = circleY * 1.01;
+circleX += circleSpeed; // short hand for circleX = circleX + circleSpeed
+circleSpeed = circleSpeed + circleAcceleration;
 ellipse(circleX, circleY, circleSize);
 }
