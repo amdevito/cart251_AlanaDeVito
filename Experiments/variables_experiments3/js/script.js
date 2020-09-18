f4b24d8846b52b0a52220b3d406cee61d5cec4a3
amdevito <amdevitoam@gmail.com>
2020-09-18 09:38:22 -0400

    Introducing Javascript Objects

---
+++ Experiments/variables_experiments3/js/script.js
@@ -0,0 +1,34 @@
+/**************************************************
+Variables Experiment 2 -
+Animated piece: Black box with
+Alana DeVito
+
+**************************************************/
+let backgroundShade = 0;
+let circleX = 250;
+let circleY = 250;
+let circleSize = 400; //state variable before set up
+let circleSpeed = -4; // have the negative in the variable not in the arguement
+let circleAcceleration = 0.25;
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() { /// writing 1/60th a second 'draw loop' or 'update loop'
+//background(backgroundShade + 2); //activate these and you lose the ripple effect but get the backgroun changing from black to white and the green circle shoots off into the distance
+//backgroundShade = backgroundShade + 2;
+strokeWeight(3);
+stroke(0, 255, 0);
+circleSize = circleSize * 0.93; // keeps moving because program keeps looping around to it
+circleY = circleY * 1.01;
+circleX += circleSpeed; // short hand for circleX = circleX + circleSpeed
+circleSpeed = circleSpeed + circleAcceleration;
+ellipse(circleX, circleY, circleSize);
+}
