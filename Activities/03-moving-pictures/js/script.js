a9e9d11c656fbadd6fe1639091da0599a4c81967
amdevito <amdevitoam@gmail.com>
2020-09-18 13:16:49 -0400

    Started Moving Pictures activity

---
+++ Activities/03-moving-pictures/js/script.js
@@ -0,0 +1,19 @@
+/**************************************************
+Activity 3: Moving Pictures
+Alana DeVito
+
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
