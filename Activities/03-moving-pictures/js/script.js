b94ba9caa80e76fa8bdbeb914d30da54a8931a90
amdevito <amdevitoam@gmail.com>
2020-09-19 22:14:27 -0400

    trying to use windowWidth...

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -3,11 +3,6 @@ Activity 3: Moving Pictures
 Alana DeVito
 
 **************************************************/
-function setup() {
-createCanvas(windowWidth, windowHeight);
-noStroke();
-}
-
 let bg = {
   red: 0,
   green: 0,
@@ -16,23 +11,30 @@ let bg = {
 let circle1 = {
   x: 0,
   y: windowHeight / 2,
-  size: windowHeight / 4,
-  speed: windowWidth / 60,
-  trans: 170,
-  //colour: (1, 119, 181) //try this colour for the circles at some point
+  size: windowWidth / 4,
+  speed: windowWidth / 7650, // 255 / 30 =7650 , so the colour spectrum divided by half the frame speed
+  colour: (1, 119, 181, 170) //try this colour for the circles at some point
 };
 let circle2 = {
   x: windowWidth,
-  y: widowHeight / 2,
+  y: windowWidth / 2,
   size: windowHeight / 6,
-  speed: windowWidth / 60,
+  speed: windowWidth / 7650, // 255 / 30 =7650 , so the colour spectrum divided by half the frame speed
   //colour: (1, 119, 181)
 };
 
-
+function setup() {
+createCanvas(windowWidth, windowHeight);
+noStroke();
+}
 
 function draw() {
-background(bg.red + 1 , bg.green, bg.blue);
+background(bg.red, bg.green, bg.blue);
+bg.red = bg.red + windowWidth / 7650;
+circle(circle1.x + windowWidth / 7650, circle1.y, circle.size + windowWidth /7650);
+fill(1, 119, 181, 170);
+
+//map(circle1.speed, 0, windowWidth / 60, 0, 255);
 
 
 }
