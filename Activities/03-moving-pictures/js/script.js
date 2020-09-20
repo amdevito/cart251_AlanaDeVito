17a52ec0669898af43a191cb1e74fe3c8dd6cf4a
amdevito <amdevitoam@gmail.com>
2020-09-19 23:29:01 -0400

    trying to use windowWidth and windowHeight to have a reactive animation
    Doesnt work. Ask why? No error message this time, but sometimes says that cannot use the p5.js function outside of setup. How do I make this work?

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -5,8 +5,11 @@ Alana DeVito
 ** ask***
 **************************************************/
 
+let windowWidth;
+let windowHeight;
+
 function setup() {
-createCanvas(500, 500);
+createCanvas(windowWidth, windowHeight);
 noStroke();
 }
 let bg = {
@@ -16,15 +19,15 @@ let bg = {
 };
 let circle1 = {
   x: 0,
-  y: 250,
-  size: 25,
+  y: windowHeight / 2,
+  size: windowWidth / 4,
   speed: 1, // 255 / 30 =7650 , so the colour spectrum divided by half the frame speed
   //color: (1, 119, 181, 170) //try this colour for the circles at some point
 };
 let circle2 = {
-  x: 500,
-  y: 250,
-  size: 5,
+  x: windowWidth,
+  y: windowHeight / 2,
+  size: windowWidth / 6,
   speed: -1, // 255 / 30 =7650 , so the colour spectrum divided by half the frame speed
   //colour: (1, 119, 181)
 };
@@ -38,16 +41,16 @@ bg.red = constrain(bg.red, 0, 255);
 fill(1, 119, 181, 170);
 ellipse(circle1.x,circle1.y,circle1.size);
 circle1.x = circle1.x + circle1.speed;
-circle1.x = constrain(circle1.x, 0, 250);
+circle1.x = constrain(circle1.x, 0, windowWidth / 2);
 circle1.size = circle1.size + 1;
-circle1.size = constrain(circle1.size, 25, 1000);
+circle1.size = constrain(circle1.size, windowWidth / 4, windowWidth * 2);
 
 fill(1, 119, 181);
 ellipse(circle2.x,circle2.y,circle2.size);
 circle2.x = circle2.x + circle2.speed;
-circle2.x = constrain(circle2.x, 250, 500);
+circle2.x = constrain(circle2.x, windowWidth / 2, windowWidth);
 circle2.size = circle2.size + 0.5;
-circle2.size = constrain(circle2.size, 25, 500);
+circle2.size = constrain(circle2.size, windowWidth / 6, windowWidth / 2);
   //circle1.x = circle1.x + windowWidth / 7650;
   //circle1.size = circle1.size + windowWidth /7650;
 //fill(1, 119, 181);
