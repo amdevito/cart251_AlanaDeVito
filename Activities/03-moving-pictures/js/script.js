d5a038e28a60b3abe02de116111a554d3c10af9f
amdevito <amdevitoam@gmail.com>
2020-09-19 21:48:41 -0400

    Trying to use windowWidth, windowHeight

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -3,17 +3,37 @@ Activity 3: Moving Pictures
 Alana DeVito
 
 **************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+createCanvas(windowWidth, windowHeight);
+noStroke();
 }
 
-// draw()
-//
-// Description of draw() goes here.
+let bg = {
+  red: 0,
+  green: 0,
+  blue: 0
+};
+let circle1 = {
+  x: 0,
+  y: windowHeight / 2,
+  size: windowHeight / 4,
+  speed: windowWidth / 60,
+  trans: 170,
+  //colour: (1, 119, 181) //try this colour for the circles at some point
+};
+let circle2 = {
+  x: windowWidth,
+  y: widowHeight / 2,
+  size: windowHeight / 6,
+  speed: windowWidth / 60,
+  //colour: (1, 119, 181)
+};
+
+
+
 function draw() {
+background(bg.red + 1 , bg.green, bg.blue);
+
 
 }
+//(map(0, windowWidth / 60, 0, 255)
