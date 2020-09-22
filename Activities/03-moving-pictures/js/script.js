3b756efc3b9047073e973c6be97ff787fa419c94
amdevito <amdevitoam@gmail.com>
2020-09-22 18:28:55 -0400

    E1: Final Piece

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -2,11 +2,10 @@
 Activity 3: Moving Pictures
 Alana DeVito
 
-Circles and ripples moving across the canvas,
-ripples change colour as you swirl your mouse around the screen.
-Floating boxes trace lines and change colour while swirling mouse as well.
-
-
+Circles with ripples moving across the canvas.
+Ripples change colour as you swirl your mouse around the screen.
+Floating boxes trace lines and change colour with swirling mouse as well.
+Circles grow and shrink as they move across the screen.
 **************************************************/
 
 //creating all the JavaScript objects used in the scene
@@ -88,6 +87,7 @@ function draw() {
   stroke(0, mouseY, 155);
   mouseY = constrain(mouseY, 0, 255);
 
+  //interactive floating rectangles. A type of 'kelidoscope' effect
   rect(rect1.x, rect1.y, rect1.sizeX, rect1.sizeY);
   rect1.sizeX = rect1.sizeX + mouseX;
   rect1.sizeX = map(rect1.sizeX, 0, 800, 0, 200);
@@ -100,6 +100,7 @@ function draw() {
   mouseY = constrain(mouseY, 0, 255);
   rect(rect2.x, rect2.y, rect2.sizeX, rect2.sizeY);
 
+  //interactive floating rectangles. A type of 'kelidoscope' effect
   rect2.sizeX = rect2.sizeX + mouseX;
   rect2.sizeX = map(rect2.sizeX, 0, 800, 0, 120);
   rect2.sizeY = rect2.sizeY + mouseY;
@@ -111,6 +112,7 @@ function draw() {
   mouseY = constrain(mouseY, 0, 255);
   rect(rect3.x, rect3.y, rect3.sizeX, rect3.sizeY);
 
+  //interactive floating rectangles. A type of 'kelidoscope' effect
   rect3.sizeX = rect3.sizeX + mouseX;
   rect3.sizeX = map(rect3.sizeX, 0, 800, 0, 70);
   rect3.sizeY = rect3.sizeY + mouseY;
@@ -122,13 +124,14 @@ function draw() {
   mouseY = constrain(mouseY, 0, 255);
   rect(rect4.x, rect4.y, rect4.sizeX, rect4.sizeY);
 
+  //interactive floating rectangles. A type of 'kelidoscope' effect
   rect4.sizeX = rect4.sizeX + mouseX;
   rect4.sizeX = map(rect4.sizeX, 0, 800, 0, 70);
   rect4.sizeY = rect4.sizeY + mouseY;
   rect4.sizeY = map(rect4.sizeY, 0, 800, 0, 70);
 
   //draw circle1 - lower right circle
-  //circles move diagonally downwards, stop in middle bottom
+  //circles move diagonally downwards, stop at right side, bottom
   stroke(mouseY, 0, 155);
   mouseY = map(mouseY, 0, 800, 0, 255);
 
@@ -142,6 +145,7 @@ function draw() {
 
   fill(255, mouseX, 0, 100);
 
+  //Controlling the stroke colours changing along with the mouse swirls.
   bg.red = bg.red + mouseY;
   bg.red = map(bg.red, width, height, 0, 155);
   bg.green = bg.green + mouseY;
@@ -151,16 +155,15 @@ function draw() {
   circle(circle1.x, circle1.y, circle1.size);
 
   circle1.size = circle1.size + 5;
-  circle1.size = constrain(circle1.size, 8, 300);
+  circle1.size = constrain(circle1.size, 1, 270);
 
   //draw circle 2 - lower left circle
-  //circles move diagonally downwards, stop in middle bottom
-
+  //circles move diagonally downwards, stop at middle bottom
   stroke(0, mouseY, 155);
   mouseY = constrain(mouseY, 0, 255);
 
   circle2.x = circle2.x + circle2.speed;
-  circle2.x = constrain(circle2.x, 100, 400);
+  circle2.x = constrain(circle2.x, 100, 410);
 
   circle2.y = circle2.y + circle2.speed;
   circle2.y = constrain(circle2.y, 100, 800);
@@ -175,9 +178,10 @@ function draw() {
   circle(circle2.x, circle2.y, circle2.size);
 
   circle2.size = circle2.size + 5;
-  circle2.size = constrain(circle2.size, 8, 300);
+  circle2.size = constrain(circle2.size, 1, 270);
 
-  //circle 3 - upper left/// circles moving across from left, get small, big and continue sideways
+  //circle 3 - upper left
+  // circles moving across from left, get small, big and continue sideways
 
   stroke(0, 0, mouseY);
   mouseY = map(mouseY, 0, 800, 0, 255);
@@ -190,7 +194,7 @@ function draw() {
   circle3.speed = circle3.speed + 0.02;
 
   circle3.size = circle3.size - 3;
-  circle3.size = constrain(circle3.size, -100, 1000); // mix of constrain and size being changed by a negative number causes the animation quality
+  circle3.size = constrain(circle3.size, -100, 1000); // mix of constrain and size being changed by a negative number causes the animation quality of getting bigger then smaller and big again
 
   fill(255, bg.green, bg.blue, 100);
 
