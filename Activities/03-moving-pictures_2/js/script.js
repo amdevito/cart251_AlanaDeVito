a067bb03f03c21973fd2101462a6ae3af91cc3f0
amdevito <amdevitoam@gmail.com>
2020-09-22 14:37:11 -0400

    E1: New Version started

--- Activities/03-moving-pictures_2/js/script.js
+++ Activities/03-moving-pictures_2/js/script.js
@@ -26,6 +26,14 @@ let circle3 = {
   acceleration: 3.5,
   fill: (170, 170, 170, 170)
 }
+let circle4 = {
+  x: 700,
+  y: 200,
+  size: 450,
+  speed: .001,
+  acceleration: -3.5,
+  fill: (170, 170, 255, 170)
+}
 let bg = {
   red: 0,
   green: 0,
@@ -35,8 +43,8 @@ let bg = {
 
 function setup() {
   createCanvas(800,800);
-background(bg.red, bg.green, bg.blue, bg.alpha);
-frameRate(20);
+  background(bg.red, bg.green, bg.blue, bg.alpha);
+  frameRate(20);
 }
 
 function draw(){
@@ -44,15 +52,13 @@ function draw(){
   stroke(0, bg.green, bg.blue, bg.alpha);
   strokeWeight(2);
 
-
-
-
 //circle1
-
-
-//circles move diagonally downwards
+//circles move diagonally downwards, stop in middle bottom
   circle1.x = circle1.x + circle1.speed;
+  circle1.x = constrain(circle1.x, 400, 680);
+
   circle1.y = circle1.y + circle1.speed;
+  circle1.y = constrain(circle1.y, 400, 800);
 
   circle1.speed = circle1.speed + circle1.acceleration;
 
@@ -67,12 +73,11 @@ function draw(){
 circle(circle1.x, circle1.y, circle1.size);
 
 circle1.size = circle1.size + 5;
+circle1.size = constrain(circle1.size, 8, 300);
+
 
 //circle 2
-
-
-
-//circles move diagonally downwards
+//circles move diagonally downwards, stop in middle bottom
   circle2.x = circle2.x + circle2.speed;
   circle2.x = constrain(circle2.x, 100, 400);
 
@@ -83,8 +88,6 @@ circle1.size = circle1.size + 5;
 
   fill(255, bg.green, bg.blue, 100);
 
-  bg.red = bg.red + mouseX;
-  bg.red = map(bg.red, width, height, 0, 255);
   bg.green = bg.green + mouseY;
   bg.green = constrain(bg.green, 0, 255);
   bg.alpha = bg.alpha + mouseX;
@@ -95,26 +98,20 @@ circle2.size = circle2.size + 5;
 circle2.size = constrain(circle2.size, 8, 300);
 
 
+//circle 3 /// circles moving across from left, get small, big and continue sideways
+  circle3.x = circle3.x + circle3.speed;
+  circle3.x = constrain(circle3.x, 0, 800);
 
+  circle3.y = circle3.y + circle3.speed;
+  circle3.y = constrain(circle3.y, 0, 300);
 
-    //circle 3
+  circle3.speed = circle3.speed + .02;
 
+  circle3.size = circle3.size - 3;
+  circle3.size = constrain(circle3.size, -200, 1000); // mix of constrain and size being changed by a negative number causes the animation quality
 
-        circle3.x = circle3.x + circle3.speed;
-        circle3.x = constrain(circle3.x, 0, 800);
+  fill(circle3.fill);
 
-        circle3.y = circle3.y + circle3.speed;
-        circle3.y = constrain(circle3.y, 0, 300);
-
-        circle3.speed = circle3.speed + .02;
-
-        circle3.size = circle3.size - 3;
-        circle3.size = constrain(circle3.size, -200, 1000);
-
-
-
-        fill(circle3.fill);
-
-        circle(circle3.x, circle3.y, circle3.size);
+  circle(circle3.x, circle3.y, circle3.size);
 
 }
