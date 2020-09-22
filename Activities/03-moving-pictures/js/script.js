4375005eb7ec51d28f42fec8525f4ef883f1d74f
amdevito <amdevitoam@gmail.com>
2020-09-22 11:24:06 -0400

    some animation working

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -8,9 +8,30 @@ let circle1 = {
   x: undefined,
   y: undefined,
   size: undefined,
-  speed: 1,
-  acceleration: 15 //plus moves it right, negative moves left
-};
+  speed: .01,
+  acceleration: 3.5 //plus moves it right, negative moves left
+}
+let circle2 = {
+  x: undefined,
+  y: undefined,
+  size: undefined,
+  speed: .01,
+  acceleration: 3.5
+}
+let circle3 = {
+  x: undefined,
+  y: undefined,
+  size: undefined,
+  speed: .01,
+  acceleration: 3.5
+}
+let circle4 = {
+  x: undefined,
+  y: undefined,
+  size: undefined,
+  speed: .01,
+  acceleration: 3.5
+}
 let bg = {
   red: 0,
   green: 0,
@@ -21,38 +42,38 @@ let bg = {
 function setup() {
   createCanvas(windowWidth,windowHeight);
 background(bg.red, bg.green, bg.blue, bg.alpha);
-//  bg.red = bg.red + 100;
-  //bg.red = constrain(bg.red, 0, 155);
-//  bg.green = bg.green + mouseY;
-  //bg.green = constrain(bg.green, 0, 255);
-//  bg.alpha = bg.alpha + mouseX;
 
 }
 
 function draw(){
-/**
-    background(bg.red, bg.green, bg.blue, bg.alpha);
-    bg.red = bg.red + mouseX;
-    //bg.red = constrain(bg.red, 0, 155);
-    bg.green = bg.green + mouseY;
-  //  bg.green = constrain(bg.green, 0, 255);
-  bg.alpha = bg.alpha + mouseX;
-**/
 
   stroke(0, 255, 0);
-  strokeWeight(2);
+  strokeWeight(.7);
 
   circle1.x = width/2;
   circle1.y = height/2;
 
-  circle1.size = width / 4;
-  circle1.size = circle1.size - 2;
-
+//circles move diagonally downwards
   circle1.x = circle1.x + circle1.speed;
   circle1.y = circle1.y + circle1.speed;
 
   circle1.speed = circle1.speed + circle1.acceleration;
-  ellipse(circle1.x, circle1.y, circle1.size);
+
+  fill(bg.red, bg.green, bg.blue, bg.alpha);
+
+  bg.red = bg.red + mouseX;
+  bg.red = map(bg.red, width, height, 0, 255);
+  bg.green = bg.green + mouseY;
+  bg.green = constrain(bg.green, 0, 255);
+  bg.alpha = bg.alpha + mouseX;
+
+ellipse(circle1.x, circle1.y, circle1.size);
+
+  circle1.size = width / 4;
+  circle1.size = circle1.size + 20;
+  circle1.size = circle1.size * 0.93;
+
+
 }
 
 /***
