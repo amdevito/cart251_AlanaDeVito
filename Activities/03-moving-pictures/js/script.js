12e52feede2474b19f24d7d0c62d094ccadb4bb3
amdevito <amdevitoam@gmail.com>
2020-09-22 17:31:19 -0400

    E1: Changed rectangle sizes and colours, some animations changed

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -3,59 +3,128 @@ Activity 3: Moving Pictures
 Alana DeVito
 
 Circles and ripples moving across the canvas,
-ripple change colour as you swirl your mouse around the screen.
+ripples change colour as you swirl your mouse around the screen.
+Floating boxes trace lines and change colour while swirling mouse as well.
 
 
 **************************************************/
+let rect1 = {
+  x: 400,
+  y: 50,
+  sizeX: 50,
+  sizeY: 50,
+};
+let rect2 = {
+  x: 90,
+  y: 700,
+  sizeX: 90,
+  sizeY: 90,
+};
+let rect3 = {
+  x: 700,
+  y: 175,
+  sizeX: 50,
+  sizeY: 50,
+};
+let rect4 = {
+  x: 750,
+  y: 500,
+  sizeX: 30,
+  sizeY: 30,
+};
 let circle1 = {
   x: 400,
   y: 400,
   size: 1,
   speed: 0.01,
-  acceleration: 0.2 //plus moves it right, negative moves left
-}
+  acceleration: 0.2, //plus moves it right, negative moves left
+};
 let circle2 = {
   x: 100,
   y: 400,
   size: 1,
   speed: 0.01,
-  acceleration: 0.2
-}
+  acceleration: 0.2,
+};
 let circle3 = {
   x: 100,
   y: 300,
-  size: 450,
-  speed: .001,
+  size: 350,
+  speed: 0.001,
   acceleration: 3.5,
-  fill: (170, 170, 170, 170)
-}
+  fill: (170, 170, 170, 170),
+};
 let circle4 = {
   x: 480,
   y: 700,
   size: 350,
-  speed: .0001,
-  acceleration: -3.5,
-  fill: (170, 170, 255, 170)
-}
+  speed: 0.0001,
+  acceleration: 3.5,
+  fill: (170, 170, 255, 170),
+};
 let bg = {
   red: 0,
   green: 0,
   blue: 0,
-  alpha: 170
-}
-
+  alpha: 170,
+};
+//set canvas size and background colour and frame rate
 function setup() {
   createCanvas(800, 800);
   background(bg.red, bg.green, bg.blue, bg.alpha);
   frameRate(20);
 }
 
+//draw the shapes
 function draw() {
-
   stroke(bg.red, bg.green, bg.blue, bg.alpha);
   strokeWeight(2);
 
-  //circle1 - lower right circle
+  //draw rect1
+  rectMode(CENTER);
+  stroke(0, mouseY, 155);
+  mouseY = constrain(mouseY, 0, 255);
+
+  rect(rect1.x, rect1.y, rect1.sizeX, rect1.sizeY);
+  rect1.sizeX = rect1.sizeX + mouseX;
+  rect1.sizeX = map(rect1.sizeX, 0, 800, 0, 200)
+  rect1.sizeY = rect1.sizeY + mouseY;
+  rect1.sizeY = map(rect1.sizeY, 0, 800, 0, 100)
+
+  //draw rect2
+  rectMode(CENTER);
+  stroke(mouseY, 0, 155);
+  mouseY = constrain(mouseY, 0, 255);
+  rect(rect2.x, rect2.y, rect2.sizeX, rect2.sizeY);
+
+  rect2.sizeX = rect2.sizeX + mouseX;
+  rect2.sizeX = map(rect2.sizeX, 0, 800, 0, 120)
+  rect2.sizeY = rect2.sizeY + mouseY;
+  rect2.sizeY = map(rect2.sizeY, 0, 800, 0, 180)
+
+  //draw rect3
+  rectMode(CENTER);
+  stroke(mouseY, 0, 155);
+  mouseY = constrain(mouseY, 0, 255);
+  rect(rect3.x, rect3.y, rect3.sizeX, rect3.sizeY);
+
+  rect3.sizeX = rect3.sizeX + mouseX;
+  rect3.sizeX = map(rect3.sizeX, 0, 800, 0, 70)
+  rect3.sizeY = rect3.sizeY + mouseY;
+  rect3.sizeY = map(rect3.sizeY, 0, 800, 0, 70)
+
+  //draw rect4
+  rectMode(CENTER);
+  stroke(0, mouseY, 155);
+  mouseY = constrain(mouseY, 0, 255);
+  rect(rect4.x, rect4.y, rect4.sizeX, rect4.sizeY);
+
+  rect4.sizeX = rect4.sizeX + mouseX;
+  rect4.sizeX = map(rect4.sizeX, 0, 800, 0, 70)
+  rect4.sizeY = rect4.sizeY + mouseY;
+  rect4.sizeY = map(rect4.sizeY, 0, 800, 0, 70)
+
+  //draw circle1 - lower right circle
   //circles move diagonally downwards, stop in middle bottom
   stroke(mouseY, 0, 155);
   mouseY = map(mouseY, 0, 800, 0, 255);
@@ -81,13 +150,11 @@ function draw() {
   circle1.size = circle1.size + 5;
   circle1.size = constrain(circle1.size, 8, 300);
 
-
-  //circle 2
+  //draw circle 2 - lower left circle
   //circles move diagonally downwards, stop in middle bottom
 
   stroke(0, mouseY, 155);
   mouseY = constrain(mouseY, 0, 255);
-
 
   circle2.x = circle2.x + circle2.speed;
   circle2.x = constrain(circle2.x, 100, 400);
@@ -107,18 +174,17 @@ function draw() {
   circle2.size = circle2.size + 5;
   circle2.size = constrain(circle2.size, 8, 300);
 
+  //circle 3 - upper left/// circles moving across from left, get small, big and continue sideways
 
-  //circle 3 /// circles moving across from left, get small, big and continue sideways
-
-  stroke(0, 155, mouseY);
+  stroke(0, 55, mouseY);
   mouseY = map(mouseY, 0, 800, 0, 255);
   circle3.x = circle3.x + circle3.speed;
-  circle3.x = constrain(circle3.x, 0, 400);
+  circle3.x = constrain(circle3.x, 0, 320);
 
   circle3.y = circle3.y + circle3.speed;
   circle3.y = constrain(circle3.y, 0, 300);
 
-  circle3.speed = circle3.speed + .02;
+  circle3.speed = circle3.speed + 0.02;
 
   circle3.size = circle3.size - 3;
   circle3.size = constrain(circle3.size, -100, 1000); // mix of constrain and size being changed by a negative number causes the animation quality
@@ -126,7 +192,6 @@ function draw() {
   fill(255, bg.green, bg.blue, 100);
 
   circle(circle3.x, circle3.y, circle3.size);
-
 
   //circle 4 /// circles moving across from right, get small, big and continue sideways
 
@@ -138,10 +203,10 @@ function draw() {
   circle4.y = circle4.y + circle4.speed;
   circle4.y = constrain(circle4.y, 0, 300);
 
-  circle4.speed = circle4.speed + .02;
+  circle4.speed = circle4.speed + 0.02;
 
   circle4.size = circle4.size - 3;
-  circle4.size = constrain(circle4.size, -200, 1000); // mix of constrain and size being changed by a negative number causes the animation quality
+  circle4.size = constrain(circle4.size, -200, 1000); // mix of constrain and size being changed by a negative number causes the animation quality of getting smaller than bigger
 
   fill(255, bg.green, bg.blue, 100);
 
