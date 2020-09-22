8e2b66bbbdd4f0b27510ce7d12bb5acb28b23255
amdevito <amdevitoam@gmail.com>
2020-09-22 14:15:03 -0400

    finally getting animation in the right direction

--- Activities/03-moving-pictures_2/js/script.js
+++ Activities/03-moving-pictures_2/js/script.js
@@ -36,34 +36,15 @@ let bg = {
 function setup() {
   createCanvas(800,800);
 background(bg.red, bg.green, bg.blue, bg.alpha);
-
+frameRate(20);
 }
 
 function draw(){
 
-  stroke(0, bg.green , bg.blue, bg.alpha);
+  stroke(0, bg.green, bg.blue, bg.alpha);
   strokeWeight(2);
 
 
-  //circle 3
-
-
-      circle3.x = circle3.x + circle3.speed;
-      circle3.x = constrain(circle3.x, 0, 300);
-
-      circle3.y = circle3.y + circle3.speed;
-      circle3.y = constrain(circle3.y, 0, 300);
-
-      circle3.speed = circle3.speed + .02;
-
-      circle3.size = circle3.size - 3;
-      circle3.size = constrain(circle3.size, -200, 1000);
-
-
-
-      fill(circle3.fill);
-
-      circle(circle3.x, circle3.y, circle3.size);
 
 
 //circle1
@@ -85,7 +66,7 @@ function draw(){
 
 circle(circle1.x, circle1.y, circle1.size);
 
-  circle1.size = circle1.size + 5;
+circle1.size = circle1.size + 5;
 
 //circle 2
 
@@ -93,7 +74,10 @@ circle(circle1.x, circle1.y, circle1.size);
 
 //circles move diagonally downwards
   circle2.x = circle2.x + circle2.speed;
+  circle2.x = constrain(circle2.x, 100, 400);
+
   circle2.y = circle2.y + circle2.speed;
+  circle2.y = constrain(circle2.y, 100, 800);
 
   circle2.speed = circle2.speed + circle2.acceleration;
 
@@ -107,8 +91,30 @@ circle(circle1.x, circle1.y, circle1.size);
 
 circle(circle2.x, circle2.y, circle2.size);
 
-  circle2.size = circle2.size + 5;
+circle2.size = circle2.size + 5;
+circle2.size = constrain(circle2.size, 8, 300);
 
 
+
+
+    //circle 3
+
+
+        circle3.x = circle3.x + circle3.speed;
+        circle3.x = constrain(circle3.x, 0, 800);
+
+        circle3.y = circle3.y + circle3.speed;
+        circle3.y = constrain(circle3.y, 0, 300);
+
+        circle3.speed = circle3.speed + .02;
+
+        circle3.size = circle3.size - 3;
+        circle3.size = constrain(circle3.size, -200, 1000);
+
+
+
+        fill(circle3.fill);
+
+        circle(circle3.x, circle3.y, circle3.size);
 
 }
