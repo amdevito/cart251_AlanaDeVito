558bb6b499b01df115473e4dc9c9128df51eecf5
amdevito <amdevitoam@gmail.com>
2020-09-22 14:41:32 -0400

    E1: Settled on Design / Concept, Type of Animation
    Had difficulties getting the animation and style to do what I wanted. Initially wanted to utilie windowWidth, windowHeight resticitions to make the piece interactive with window size, but the animation wasn't working because of this.

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -5,33 +5,34 @@ Alana DeVito
 
 **************************************************/
 let circle1 = {
-  x: undefined,
-  y: undefined,
-  size: undefined,
-  speed: .01,
-  acceleration: 3.5 //plus moves it right, negative moves left
+  x: 400,
+  y: 400,
+  size: 8,
+  speed: 0.01,
+  acceleration: 0.2 //plus moves it right, negative moves left
 }
 let circle2 = {
-  x: undefined,
-  y: undefined,
-  size: undefined,
-  speed: .01,
-  acceleration: 3.5
+  x: 100,
+  y: 400,
+  size: 8,
+  speed: 0.01,
+  acceleration: 0.2
 }
 let circle3 = {
-  x: undefined,
-  y: undefined,
-  size: undefined,
-  speed: .01,
+  x: 100,
+  y: 200,
+  size: 450,
+  speed: .001,
   acceleration: 3.5,
   fill: (170, 170, 170, 170)
 }
 let circle4 = {
-  x: undefined,
-  y: undefined,
-  size: undefined,
-  speed: .01,
-  acceleration: 3.5
+  x: 700,
+  y: 200,
+  size: 450,
+  speed: .001,
+  acceleration: -3.5,
+  fill: (170, 170, 255, 170)
 }
 let bg = {
   red: 0,
@@ -41,41 +42,27 @@ let bg = {
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-background(bg.red, bg.green, bg.blue, bg.alpha);
-
+  createCanvas(800,800);
+  background(bg.red, bg.green, bg.blue, bg.alpha);
+  frameRate(20);
 }
 
 function draw(){
 
-  stroke(0, bg.green , bg.blue, bg.alpha);
-  strokeWeight(1);
+  stroke(0, bg.green, bg.blue, bg.alpha);
+  strokeWeight(2);
 
-
-  circle3.x = width / 3;
-  circle3.y = height / 1.8;
-
-  circle3.x = circle3.x + circle3.speed;
-  circle3.y = circle3.y + circle3.speed;
-
-  circle3.size = width / 1.5;
-  circle3.size = circle3.size +3;
-
-  fill(circle3.fill);
-
-  circle(circle3.x, circle3.y, circle3.size);
-
-  circle1.x = width/2;
-  circle1.y = height/2;
-
-
-//circles move diagonally downwards
+//circle1
+//circles move diagonally downwards, stop in middle bottom
   circle1.x = circle1.x + circle1.speed;
+  circle1.x = constrain(circle1.x, 400, 680);
+
   circle1.y = circle1.y + circle1.speed;
+  circle1.y = constrain(circle1.y, 400, 800);
 
   circle1.speed = circle1.speed + circle1.acceleration;
 
-  fill(bg.red, bg.green, bg.blue, bg.alpha);
+  fill(255, bg.green, bg.blue, 170);
 
   bg.red = bg.red + mouseX;
   bg.red = map(bg.red, width, height, 0, 255);
@@ -85,35 +72,46 @@ function draw(){
 
 circle(circle1.x, circle1.y, circle1.size);
 
-  circle1.size = width / 4;
-  circle1.size = circle1.size + 20;
-  circle1.size = circle1.size * 0.93;
+circle1.size = circle1.size + 5;
+circle1.size = constrain(circle1.size, 8, 300);
+
 
 //circle 2
-
-
-  circle2.x = width/5;
-  circle2.y = height/2;
-
-//circles move diagonally downwards
+//circles move diagonally downwards, stop in middle bottom
   circle2.x = circle2.x + circle2.speed;
+  circle2.x = constrain(circle2.x, 100, 400);
+
   circle2.y = circle2.y + circle2.speed;
+  circle2.y = constrain(circle2.y, 100, 800);
 
   circle2.speed = circle2.speed + circle2.acceleration;
 
-  fill(bg.red, bg.green, bg.blue, bg.alpha);
+  fill(255, bg.green, bg.blue, 100);
 
-  bg.red = bg.red + mouseX;
-  bg.red = map(bg.red, width, height, 0, 255);
   bg.green = bg.green + mouseY;
   bg.green = constrain(bg.green, 0, 255);
   bg.alpha = bg.alpha + mouseX;
 
 circle(circle2.x, circle2.y, circle2.size);
 
-  circle2.size = width / 4;
-  circle2.size = circle2.size + 20;
-  circle2.size = circle2.size * 0.93;
+circle2.size = circle2.size + 5;
+circle2.size = constrain(circle2.size, 8, 300);
 
+
+//circle 3 /// circles moving across from left, get small, big and continue sideways
+  circle3.x = circle3.x + circle3.speed;
+  circle3.x = constrain(circle3.x, 0, 800);
+
+  circle3.y = circle3.y + circle3.speed;
+  circle3.y = constrain(circle3.y, 0, 300);
+
+  circle3.speed = circle3.speed + .02;
+
+  circle3.size = circle3.size - 3;
+  circle3.size = constrain(circle3.size, -200, 1000); // mix of constrain and size being changed by a negative number causes the animation quality
+
+  fill(circle3.fill);
+
+  circle(circle3.x, circle3.y, circle3.size);
 
 }
