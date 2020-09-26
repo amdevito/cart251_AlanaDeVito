39d38c665465b6bd5e01a74afa562cb57ea77ecc
amdevito <amdevitoam@gmail.com>
2020-09-26 12:03:37 -0400

    Started loop statements

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -4,26 +4,29 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-let displayCircle = false;
-// setup()
-//
-// Description of setup() goes here.
+let caterpillar ={
+  x: 100,
+  y: 250,
+  segmentSize: 50
+}
+
 function setup() {
 createCanvas(500, 500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
+
 function draw() {
+  background(0);
+  noStroke();
+  fill(100, 200, 100);
 
-background(0);
+  let x = caterpillar.x;
+  let numSegments = 10; //how many segments you want to draw
+  let segmentsDrawn = 0; // how many have been drawn
 
-if (mouseIsPressed) {
-  displayCircle = true;
+  while (segmentsDrawn < numSegments) { //while this is true do the next instructions
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 40;
+    segmentsDrawn ++;
+  }
 }
-
-  if (displayCircle) {
-    ellipse (250, 250, 250);
-  }
-  }
