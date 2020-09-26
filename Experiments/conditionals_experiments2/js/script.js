4442d73ad01cea469a05167fc063696e8a6eefb0
amdevito <amdevitoam@gmail.com>
2020-09-26 16:47:57 -0400

    intro to push and pop

--- Experiments/conditionals_experiments2/js/script.js
+++ Experiments/conditionals_experiments2/js/script.js
@@ -1,20 +1,27 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Intermediate Drawing
+Alana
 
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
 
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(255);
 
+push(); //save the styling (so the defaul settings)
+  fill(255, 0, 0);
+  stroke(0, 255, 255);
+  strokeWeight(10);
+  rect(100, 100, 100, 100);
+pop(); //restores settings from push - note: dpo this around each shape 'style'
+
+push();
+  fill(0, 0, 255);
+  rect(300,100,100,100);
+pop();
 }
