e622db8a97caf6f46d6e95fd10403c3f9186bc12
amdevito <amdevitoam@gmail.com>
2020-09-26 08:52:17 -0400

    experimenting with nosie and random for a sec.

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -4,17 +4,27 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 10,
+  size: 100,
+  speed: 1
+}
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(500, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+  background(backgroundShade);
+  circle.x += circle.speed;
+  ellipse(circle.x, circle.y, circle.size);
+  circle.x += random(-16, 16);
+  circle.y += noise(-16, 16);
 }
