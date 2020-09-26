68887dc88a75d9de0ed1da6b7e5f20f43dee56eb
amdevito <amdevitoam@gmail.com>
2020-09-26 17:16:33 -0400

    Rectangle spins forward and gets bigger from a tiny rectangle

--- Experiments/conditionals_experiments2/js/script.js
+++ Experiments/conditionals_experiments2/js/script.js
@@ -4,6 +4,8 @@ Alana
 
 Here is a description of this template p5 project.
 **************************************************/
+let angle = 0;
+let rectScale = 0;
 
 function setup() {
   createCanvas(500, 500);
@@ -11,22 +13,20 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(170);
 
 push();
-  fill(255, 0, 0);
-  rect(0,0, 100, 100);
+fill(255,0,0);
+rectMode(CENTER);
+translate(width/2,height/2); //moves origin to center
+//rotate(radians(45));
+rotate(angle);
+scale(rectScale);//scale to different size
+rect(0,0,100,100);
+//rect(width/2,height/2,250,100,100); this gave a cooool weird unexpected result with a rectangle with 2 circlular ends
+
 pop();
 
-push();
-  translate(200, 100); ///moved the ORIGIN over 200 px. and down 100 px. // translate is good for 3D animation!
-  fill(0, 255, 0);
-  rect(0,0,100,100);
-pop();
-
-push();
-  translate(0, 200); //because of push pop, translate is reset
-  fill(0,0,255);
-  rect(0,0,100,100);
-pop();
+angle += 1; //spins on the origin
+rectScale = rectScale + 0.01
 }
