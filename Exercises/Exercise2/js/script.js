3e52ddfdbb2a28218fc3518c9b1dc04c33bdcf37
amdevito <amdevitoam@gmail.com>
2020-09-27 19:37:43 -0400

    E2: Scaled all images and objects, arranged everything on canvas. Commented next steps in design.

--- Exercises/Exercise2/js/script.js
+++ Exercises/Exercise2/js/script.js
@@ -1,6 +1,10 @@
 /**************************************************
 DOdGing DOGS: Adventures of the White Squrriel
 **************************************************/
+/// NEXT STEPS: make acrons move across bottom of screen (left to right), then rotate as they move
+//make dogs move across screen (right to left) and rock back and forth as they do.
+//then ake logic that stops game when dog hits squirrel - or eventually 'releases acorns' back to bottom of the screen.(maybe acrons dont need to move, they can just spin in place? and then float to top of screen when squirrel touches them, and they drop back down if dog touches squirrel
+
 
 // dogs come in from the right at different distances outside of the frame so they are staggerd. so x= 1200 + randomNumber(1200, 1300, y position is randomNumber(height/2, height) midway to bottom
 let bg;
@@ -78,91 +82,85 @@ background(bg);
 
 push(); //must do push and pop so that the squirrel doesnt leave trail on screen
   imageMode(CENTER);
-  image(squirrelImage, mouseX, mouseY);
+  image(squirrelImage, mouseX, mouseY, 150, 50);
 pop();
 
 //acrons rool across bottom of the screen. when squirrel touches ones it floats to the top of the screen (like keeping score.)
 push();
 imageMode(CENTER);
-image(acorn1, 10, 700);
+image(acorn1, 10, 775, 30, 50);
 pop();
 
 
 push();
 imageMode(CENTER);
-image(acorn2, 200, 700);
+image(acorn2, 200, 775, 30, 50);
 pop();
 
 push();
 imageMode(CENTER);
-image(acorn1, 400, 700);
-pop();
-
-
-push();
-imageMode(CENTER);
-image(acorn2, 600, 700);
-pop();
-
-push();
-imageMode(CENTER);
-image(acorn1, 800, 700);
+image(acorn1, 400, 775, 30, 50);
 pop();
 
 
 push();
 imageMode(CENTER);
-image(acorn2, 1000, 700);
+image(acorn2, 600, 775, 30, 50);
 pop();
 
 push();
 imageMode(CENTER);
-image(acorn1, 1200, 700);
-pop();
-
-
-push();
-imageMode(CENTER);
-image(acorn2, 1400, 700);
-pop();
-
-//DOGS
-
-push();
-imageMode(CENTER);
-image(dog1, 1400, 500);//randomize these (for all dogs)
-pop();
-
-push();
-imageMode(CENTER);
-image(dog2, 1200, 500);
+image(acorn1, 800, 775, 30, 50);
 pop();
 
 
 push();
 imageMode(CENTER);
-image(dog3, 1000, 500);
+image(acorn2, 1000, 775, 30, 50);
 pop();
 
 push();
 imageMode(CENTER);
-image(dog4, 600, 500);
-pop();
-
-push();
-imageMode(CENTER);
-image(dog2, 400, 500);
+image(acorn1, 1200, 775, 30, 50);
 pop();
 
 
 push();
 imageMode(CENTER);
-image(dog1, 200, 500);
+image(acorn2, 1400, 775, 30, 50);
+pop();
+
+//DOGS /// have all of their stateing positions be different so that they are staggared asthe mmove accross the screen.
+
+push();
+imageMode(CENTER);
+image(dog2, 1400, 600, 200, 174);//randomize these (for all dogs)
+pop();
+
+push();
+imageMode(CENTER);
+image(dog3, 1200, 650, 100, 86);
+pop();
+
+
+push();
+imageMode(CENTER);
+image(dog3, 1000, 600, 100, 86);
+pop();
+
+push();
+imageMode(CENTER);
+image(dog4, 690, 660, 175, 178);
+pop();
+
+
+push();
+imageMode(CENTER);
+image(dog1, 500, 600, 200, 174);
 pop();
 
 
 }
-///next rolling acorns
 
 
 ///once i need to get the key up and key down function to move the squirrel
