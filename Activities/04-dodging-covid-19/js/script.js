75afc2e0171d4ea11b143743c618513734220883
amdevito <amdevitoam@gmail.com>
2020-09-27 14:51:48 -0400

    A4: set variables/objects

--- Activities/04-dodging-covid-19/js/script.js
+++ Activities/04-dodging-covid-19/js/script.js
@@ -1,20 +1,49 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+DOdGing DOGS: Adventures with the White Squrriel
 **************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
+let bg;
+let dog1 = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ax: 0
+}
+let dog2 = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ax: 0
+}
+let dog3 = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ax: 0
+}
+let dog4 = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ax: 0
+}
 function setup() {
+  // The background image must be the same size as the parameters
+  // into the createCanvas() method. In this program, the size of
+  // the image is 720x400 pixels.
+  bg = loadImage('assets/images/park.jpg');
+  createCanvas(1400, 800);
+
 
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
-
+  background(bg);
 }
