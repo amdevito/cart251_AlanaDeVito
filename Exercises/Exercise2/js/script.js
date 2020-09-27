9f0674bce6f2bda33818ce2e764cc88666999eed
amdevito <amdevitoam@gmail.com>
2020-09-27 18:07:21 -0400

    E2: SQUIRREL IS IN THE PARK!
    Squirrel is moving in the park

--- Exercises/Exercise2/js/script.js
+++ Exercises/Exercise2/js/script.js
@@ -3,12 +3,15 @@ DOdGing DOGS: Adventures of the White Squrriel
 **************************************************/
 
 // dogs come in from the right at different distances outside of the frame so they are staggerd. so x= 1200 + randomNumber(1200, 1300, y position is randomNumber(height/2, height) midway to bottom
-
+let bg;
 let squirrelImage;
+
 
 
 function preload() {
   squirrelImage = loadImage("assets/images/squirrel.png");
+  bg = loadImage('assets/images/park.jpg');
+
 }
 
 function setup() {
@@ -19,9 +22,15 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-
+  background(bg);
+push();
   imageMode(CENTER);
   image(squirrelImage, mouseX, mouseY);
-
+pop();
 }
+
+
+///once i need to get the key up and key down function to move the squirrel
+// function keyPressed() {
+//   if (keyCode === UP_ARROW) {
+    //x++
