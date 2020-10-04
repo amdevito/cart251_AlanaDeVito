633e84ed83129aa6c027d8a8fd78d6e5a4198fb0
amdevito <amdevitoam@gmail.com>
2020-10-03 22:12:59 -0400

    AOTWS Star Wars like title screen with font options

--- Experiments/functions_experiments5/js/script.js
+++ Experiments/functions_experiments5/js/script.js
@@ -6,30 +6,45 @@ Functions Experiments5
 Text text text text text
 **************************************************/
 
-let hello = {
+let squirrelTitle = {
   string: `Adventures of the White Squirrel`,
-  x: width/2,
-  y: height/2,
-  vx: height/width*5,
-  vy: height/width
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  size: 0
 };
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+    squirrelTitle.x = width/2;
+    squirrelTitle.y = height;
+    squirrelTitle.vx = height/width*5;
+    squirrelTitle.vy = height/width;
+    squirrelTitle.size = width/height*50;
+     // height/width gives one unit of the canvas size. Multiply by the value you want//
 }
 
 function draw() {
   background(0);
 
+
+  // squirrelTitle.x += width/height;
+  squirrelTitle.y += -width/height*0.8;
+
+  squirrelTitle.size = squirrelTitle.size - (width/height*0.1);
+
   textAlign(CENTER, CENTER);
-  textSize(width/20);
+  textSize(squirrelTitle.size);
   textStyle(BOLD);
+  textFont('Luminari');// font options: Arial, Verdana, Trebuchet MS, Times New Roman, Didot, American Typewriter, Andale Mono, Courier, Bradley Hand, Luminari ~ Sans-serif, serif, Monospace, Cursive, Fantasy, Impact, Trattatello
 
   fill(random(0, 255),random(0,255), random(0,255));
   stroke(50, 200, 50);
-  strokeWeight(width/height*4);
+  strokeWeight(width/height*1);
 
-  text(`Adventures of the White Squirrel`, width/2, height/2); //(text in ``, x, y)
+  text(squirrelTitle.string, squirrelTitle.x, squirrelTitle.y); //(text in ``, x, y)
 }
 // //function unit(u) = {
 //   let unit = height/width * u;
