b593efef7f618c1ae112fdfc85166a487879bfb9
amdevito <amdevitoam@gmail.com>
2020-10-04 09:47:14 -0400

    A5: Set functions

--- Activities/05-looking-for-love/js/script.js
+++ Activities/05-looking-for-love/js/script.js
@@ -49,27 +49,43 @@ circle2.vy = random(-circle2.speed, circle2.speed);
 // Description of draw() goes here.
 function draw() {
   background(0);
-//move the circles
-  circle1.x += circle1.vx;
-  circle1.y += circle1.vy;
 
-  circle2.x += circle2.vx;
-  circle2.y += circle2.vy;
+  move();
+  checkOffScreen();
 
-//check if the circles have gone offscreen
-if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
-  //SAD ENDING
+  checkOverlap();
+  display();
 }
 
-//check if the circles overlap
+function move() {
+  //move the circles
+    circle1.x += circle1.vx;
+    circle1.y += circle1.vy;
 
-let d = dist(circle1.x, circle2.y, circle2.x, circle2.y);
-if (d < circle1.size/2 + circle2.size/2) {
-  // Love ENDING!
+    circle2.x += circle2.vx;
+    circle2.y += circle2.vy;
 }
 
-//display circles
-  ellipse(circle1.x, circle1.y, circle1.size);
-  ellipse(circle2.x, circle2.y, circle2.size);
+function checkOffScreen() {
 
+  //check if the circles have gone offscreen
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
+    //SAD ENDING
+  }
+}
+
+function checkOverlap() {
+  //check if the circles overlap
+
+  let d = dist(circle1.x, circle2.y, circle2.x, circle2.y);
+  if (d < circle1.size/2 + circle2.size/2) {
+    // Love ENDING!
+  }
+}
+
+function display() {
+
+  //display circles
+    ellipse(circle1.x, circle1.y, circle1.size);
+    ellipse(circle2.x, circle2.y, circle2.size);
 }
