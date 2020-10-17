5728edc4b1104123784ef4497b571cb71907d6a6
amdevito <amdevitoam@gmail.com>
2020-10-17 12:47:27 -0400

    Sound rates in arrays!

--- Experiments/arrays2/js/script.js
+++ Experiments/arrays2/js/script.js
@@ -89,4 +89,91 @@ Arrays2
 //     // If we've gone past the end, go back one to the last real element
 //     currentIndex = 0;
 //   }
+//
+
+//
+// "use strict";
+//
+// let circle = {
+//   x: 0,
+//   y: 0,
+//   size: 100,
+//   trail: [],
+//   trailSize: 20 // Note that we are creating an EMPTY TRAIL ARRAY as a PROPERTY of the circle
 // }
+//
+// // setup() the canvas ready
+// function setup() {
+//   createCanvas(600, 600);
+// }
+//
+// // draw() draws a circle with a trails
+// function draw() {
+//   background(0);
+//   fill(random(0, 255), random(0, 100), random(0, 255));
+//
+//   // Use a for loop to go through each element in the circle's trail array in order
+//   for (let i = 0; i < circle.trail.length; i++) {
+//     // Get the element at the index indicated by i (0, then 1, then 2, etc.)
+//     let element = circle.trail[i];
+//     // Draw an ellipse the same size as the circle at that position
+//     ellipse(element.x, element.y, circle.size);
+//   }
+//
+//   // Move the circle to the mouse position
+//   circle.x = mouseX;
+//   circle.y = mouseY;
+//
+//   // Draw the circle
+//   ellipse(circle.x, circle.y, circle.size);
+//
+//   // Create a new position object that stores where the circle is now
+//   // which we can add to the trail to trace the path of the circle
+//   let newTrailPosition = {
+//     x: circle.x,
+//     y: circle.y
+//   };
+//   // Add the position to the circle's trail array
+//   circle.trail.push(newTrailPosition);
+//
+//   if (circle.trail.length > circle.trailSize) {
+//     circle.trail.shift(); //remove the element at 0 and re-shuffle elements
+//   }
+// }
+///
+//arays 2 part 3
+
+"use strict";
+
+// An array of different playback rates to use with our dog bark
+// 1 means normal rate, greater than one increases the speed and pitch
+// So 3 is three times faster with a correspondingly higher pitch.
+let rates = [1.5, 1.75, 2.25, 2.5, 2.75, 3];
+
+// Our sound effect
+let barkSFX;
+
+// preload() loads the barking sound effect
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
+
+// setup() the canvas ready
+function setup() {
+  createCanvas(600, 600);
+}
+
+// draw() does nothing much
+function draw() {
+  background(0);
+}
+
+// mousePressed() plays a dog bark at a randomly chosen rate
+function mousePressed() {
+  // Choose a random rate from the array
+  let barkRate = random(rates);
+  // Set the barking sound effect to that rate
+  barkSFX.rate(barkRate);
+  // Play the barking sound effect
+  barkSFX.play();
+}
