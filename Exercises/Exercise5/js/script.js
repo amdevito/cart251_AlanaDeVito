054528a02c0e4f322d4c80298dd34fe2ea429004
amdevito <amdevitoam@gmail.com>
2020-10-25 10:37:47 -0400

    E5: Started

---
+++ Exercises/Exercise5/js/script.js
@@ -0,0 +1,16 @@
+/**************************************************
+Ex. 5
+Am DeVito
+
+interactive soundscape
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
