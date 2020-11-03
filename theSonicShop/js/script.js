ce9fd409da9cb989c91fb8c0855cc7f807f462b5
amdevito <amdevitoam@gmail.com>
2020-11-03 15:32:34 -0500

    Prototype: Unable to pass welcome display 1

--- theSonicShop/js/script.js
+++ theSonicShop/js/script.js
@@ -7,38 +7,45 @@ Alana DeVito
 let sonicSign = {
   x: 0,
   y: 0,
+  video: undefined,
 };
 
 let boxOffice;
 
 let boxOffice1 = {
-  x: 1000,
-  y: 1000,
+  x: 0,
+  y: 0,
+  image: undefined,
 };
 
 let boxOffice2 = {
-  x: 1000,
-  y: 1000,
+  x: 0,
+  y: 0,
+  image: undefined,
 };
 
 let boxOffice3 = {
-  x: 1000,
-  y: 1000,
+  x: 0,
+  y: 0,
+  image: undefined,
 };
 
 let boxOffice4 = {
-  x: 1000,
-  y: 1000,
+  x: 0,
+  y: 0,
+  image: undefined,
 };
 
 let boxOffice5 = {
-  x: 1000,
-  y: 1000,
+  x: 0,
+  y: 0,
+  image: undefined,
 };
 
 let boxOffice6 = {
-  x: 1000,
-  y: 1000,
+  x: 0,
+  y: 0,
+  image: undefined,
 };
 
 let gallery;
@@ -49,42 +56,49 @@ function preload() {
   //background
   boxOffice = loadImage("assets/images/box_office1_intro.png"); //boxoffice background
   // box office interaction screens
-  boxOffice1 = loadImage("assets/images/box_office1_1.png"); //intro1
-  boxOffice2 = loadImage("assets/images/box_office1_2.png"); //intro2
-  boxOffice3 = loadImage("assets/images/box_office1_3.png"); //intro3
-  boxOffice4 = loadImage("assets/images/box_office1_4.png"); //intro4
-  boxOffice5 = loadImage("assets/images/box_office1_5.png"); //intro5
-  boxOffice6 = loadImage("assets/images/box_office1_6.png"); //intro6
+  boxOffice1.image = loadImage("assets/images/box_office1_1.png"); //intro1
+  boxOffice2.image = loadImage("assets/images/box_office1_2.png"); //intro2
+  boxOffice3.image = loadImage("assets/images/box_office1_3.png"); //intro3
+  boxOffice4.image = loadImage("assets/images/box_office1_4.png"); //intro4
+  boxOffice5.image = loadImage("assets/images/box_office1_5.png"); //intro5
+  boxOffice6.image = loadImage("assets/images/box_office1_6.png"); //intro6
 
   //Gallery
   gallery = loadImage("assets/images/gallery_render.png"); //gallery
+  sonicSign.video = createVideo(
+    ["assets/video/gallery_sign.mov"],
+    sonicSignLoad
+  );
 }
 // Description of setup() goes here.
 function setup() {
-  createCanvas(900, 1000);
-  sonicSign = createVideo(["assets/video/gallery_sign.mov"], sonicSignLoad);
-  sonicSign.size(609, 188);
+  createCanvas(800, 1000);
+
+  sonicSign.video.size(409, 78);
+  sonicSign.video.hide();
+  sonicSign.x = width / 2;
+  sonicSign.y = height / 2 - 380;
 }
 
 function sonicSignLoad() {
-  sonicSign.loop();
-  sonicSign.volume(0);
+  sonicSign.video.loop();
+  sonicSign.video.volume(0);
+  //  sonicSign.play();
 }
 
 function welcomeDisplay() {
   push();
   background(boxOffice);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
 }
 
 function welcomeDisplay1() {
   push();
-  background(boxOffice);
+  background(boxOffice1.image);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
-  image(boxOffice1, boxOffice1.x, boxOffice1.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
   //controls for this screen
   if (keyIsPressed) {
@@ -98,10 +112,9 @@ function welcomeDisplay1() {
 
 function welcomeDisplay2() {
   push();
-  background(boxOffice);
+  background(boxOffice2.image);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
-  image(boxOffice2, boxOffice2.x, boxOffice2.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
   //controls for this screen
   if (keyIsPressed) {
@@ -118,10 +131,9 @@ function welcomeDisplay2() {
 
 function welcomeDisplay3() {
   push();
-  background(boxOffice);
+  background(boxOffice3.image);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
-  image(boxOffice3, boxOffice3.x, boxOffice3.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
   //controls for this screen
   if (keyIsPressed) {
@@ -138,10 +150,9 @@ function welcomeDisplay3() {
 
 function welcomeDisplay4() {
   push();
-  background(boxOffice);
+  background(boxOffice4.image);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
-  image(boxOffice4, boxOffice4.x, boxOffice4.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
   //controls for this screen
   if (keyIsPressed) {
@@ -155,10 +166,9 @@ function welcomeDisplay4() {
 
 function welcomeDisplay5() {
   push();
-  background(boxOffice);
+  background(boxOffice5.image);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
-  image(boxOffice5, boxOffice5.x, boxOffice5.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
   //controls for this screen
   if (keyIsPressed) {
@@ -172,10 +182,9 @@ function welcomeDisplay5() {
 
 function welcomeDisplay6() {
   push();
-  background(boxOffice);
+  background(boxOffice6.image);
   imageMode(CENTER);
-  image(sonicSign, sonicSign.x, sonicSign.y);
-  image(boxOffice6, boxOffice6.x, boxOffice6.y);
+  image(sonicSign.video, sonicSign.x, sonicSign.y, 600, 180);
   pop();
   //controls for this screen
   if (keyIsPressed) {
@@ -208,8 +217,6 @@ function backHome() {
 //
 // Description of draw() goes here.
 function draw() {
-  welcomeDisplay();
-
   //set up the different states
   if (state === `enter`) {
     welcomeDisplay();
