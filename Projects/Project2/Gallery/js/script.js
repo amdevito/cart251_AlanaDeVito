a9bf349d9b47a5b7cb3cdca2cefe659da51b2837
amdevito <amdevitoam@gmail.com>
2020-11-19 22:10:15 -0500

    E7: fucntions for displaying patrons up need to add values to x, y and sizes to display.

--- Projects/Project2/Gallery/js/script.js
+++ Projects/Project2/Gallery/js/script.js
@@ -5,6 +5,8 @@ Exercise 7 : Progress report
 2. Gifs/digiCanvases when clicked take you to that sound installation.
 3. Gallery Patrons are moving around the gallery
 4. When you click on that Patron, you are taken into the hyperlink narrative
+
+need to make canvases clickable? check overlap and mouse press?
 **************************************************/
 "use strict";
 
@@ -21,6 +23,8 @@ let personLongHair = {
   maxX: 0,
   minY: 0,
   maxY: 0,
+  sizeX: 0,
+  sizeY: 0,
   image: 0,
   ax: 0,
   ay: 0,
@@ -35,6 +39,8 @@ let personShortHair = {
   maxX: 0,
   minY: 0,
   maxY: 0,
+  sizeX: 0,
+  sizeY: 0,
   image: 0,
   ax: 0,
   ay: 0,
@@ -49,6 +55,8 @@ let childParent = {
   maxX: 0,
   minY: 0,
   maxY: 0,
+  sizeX: 0,
+  sizeY: 0,
   image: 0,
   ax: 0,
   ay: 0,
@@ -105,29 +113,22 @@ function preload() {
   sonicSign.image = loadImage("assets/images/theSonicShop.gif");
 
   //load animated, clickable  digiCanvases
-  digiCanvas1.image = loadImage("assets/images/digiCanvas1.gif"); //intro2
+  digiCanvas1.image = loadImage("assets/images/digiCanvas1.gif");
   digiCanvas2.image = loadImage("assets/images/digiCanvas2.gif");
-  // // Load all the gifs (animated digiCanvases) into the array
-  // for (let i = 0; i < 3; i++) {
-  //   let patron = loadImage(`assets/images/patron${i}.png`); // get gif from file and push into array position
-  //   patrons.push(patron);
-  // }
+
+  //load patron pictures
+  personLongHair.image = loadImage("assets/images/patron0.png");
+  personShortHair.image = loadImage("assets/images/patron1.png");
+  childParent.image = loadImage("assets/images/patron2.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1125, 725);
   sonicSign.x = width / 2 - 230;
   sonicSign.y = height / 2 - 270;
 
-  digiCanvas1.x = width / 2 - 229; ///eventually change the size of the sides to make it on an angle so that it 'seems' in perspective
-  digiCanvas1.y = height / 2 + 10;
-  digiCanvas1.sizeX = 114;
-  digiCanvas1.sizeY = 95;
-
-  digiCanvas2.x = width / 2 + 315;
-  digiCanvas2.y = height / 2 + 8;
-  digiCanvas2.sizeX = 185;
-  digiCanvas2.sizeY = 115;
+  setUpCanvases();
+  setUpPatrons();
 
   // for (let i = 0; i < 3; i++) {
   //   let x = 0;
@@ -153,6 +154,13 @@ function setup() {
 //
 function draw() {
   background(0);
+  image(
+    personLongHair.image,
+    personLongHair.x,
+    personLongHair.y,
+    personLongHair.sizeX,
+    personLongHair.sizey
+  );
 
   //place gallery image on screen
   imageMode(CENTER);
@@ -167,29 +175,132 @@ function draw() {
   );
   rotate(0.03, 0); // tilt the gif slightly to place in digiCanvas properly. How do you do this?
   image(
+    //put into display function
     digiCanvas2.image,
     digiCanvas2.x,
     digiCanvas2.y,
     digiCanvas2.sizeX,
     digiCanvas2.sizeY
   );
+
+  simulation(); // this may work once the values are iin
   //
   // for (let i = 0; i < 3; i++) {
   //   digiCanvases[i].display();
 }
 //
 
+//need to assign values here to make things appear
 function setUpPatrons() {
   personLongHair.x = random(personLongHair.minY, personLongHair.maxY);
   personLongHair.y = random(personLongHair.minY, personLongHair.maxY);
   personLongHair.vx = personLongHair.speed;
 
   personShortHair.x = random(personShortHair.minY, personShortHair.maxY);
-  personShortShortHair.y = random(personShortHair.minY, personShortHair.maxY);
+  personShortHair.y = random(personShortHair.minY, personShortHair.maxY);
   personShortHair.vx = personShortHair.speed;
 
   childParent.x = random(childParent.minY, childParent.maxY);
   childParent.y = random(childParent.minY, childParent.maxY);
   childParent.vx = childParent.speed;
 }
-// digiCanvases.push(new digiCanvas(gifs[0], width / 3, height / 2, 100, 200));
+
+function setUpCanvases() {
+  /// need to do a display object too
+  digiCanvas1.x = width / 2 - 229; ///eventually change the size of the sides to make it on an angle so that it 'seems' in perspective
+  digiCanvas1.y = height / 2 + 10;
+  digiCanvas1.sizeX = 114;
+  digiCanvas1.sizeY = 100;
+
+  digiCanvas2.x = width / 2 + 315;
+  digiCanvas2.y = height / 2 + 8;
+  digiCanvas2.sizeX = 185;
+  digiCanvas2.sizeY = 115;
+}
+
+function simulation() {
+  move();
+  checkMouseHoover();
+  display();
+}
+
+///
+
+function move() {
+  ///patrons moving around in thier assigned spaces - next make them switch spaces, more freedom? or just trace logic through the gallery missing gallery walls
+  personLongHair.x += personLongHair.vx;
+  personLongHair.y += personLongHair.vy;
+
+  if (personLongHair.x < 0) {
+    personLongHair.x = personLongHair.minX;
+    personLongHair.y = random(personLongHair.minY, personLongHair.maxY);
+  }
+
+  personShortHair.x += personShortHair.vx;
+  personShortHair.y += personShortHair.vy;
+
+  if (personShortHair.x < 0) {
+    personShortHair.x = personShortHair.minX;
+    personShortHair.y = random(personShortHair.minY, personShortHair.maxY);
+  }
+
+  childParent.x += childParent.vx;
+  childParent.y += childParent.vy;
+
+  if (childParent.x < 0) {
+    childParent.x = childParent.minX;
+    childParent.y = random(childParent.minY, randomDogSpawn.maxY);
+  }
+}
+///hoover over a character to engage in a dialogue
+function checkMouseHoover() {
+  let d1 = dist(mouseX, mouseY, personLongHair.x, personLongHair.y);
+  if (d1 < 75) {
+    state = `narrative1`; //need to link to current beginning for now. will be the hyperlink narrative
+  }
+  let d2 = dist(mouseX, mouseY, personShortHair.x, personShortHair.y);
+  if (d2 < 75) {
+    state = `narrative2`;
+  }
+  let d3 = dist(mouseX, mouseY, childParent.x, childParent.y);
+  if (d3 < 75) {
+    state = `narrative3`;
+  }
+}
+
+function display() {
+  //display patron with long hair
+  push();
+  imageMode(CENTER);
+  image(
+    personLongHair.image,
+    personLongHair.x,
+    personLongHair.y,
+    personLongHair.sizeX,
+    personLongHair.sizey
+  );
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(
+    personShortHair.image,
+    personShortHair.x,
+    personShortHair.y,
+    personShortHair.sizeX,
+    personShortHair.sizey
+  );
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(
+    childParent.image,
+    childParent.x,
+    childParent.y,
+    childParent.sizeX,
+    childParent.sizey
+  );
+}
+
+//how to i make the patrons a link/clickable
