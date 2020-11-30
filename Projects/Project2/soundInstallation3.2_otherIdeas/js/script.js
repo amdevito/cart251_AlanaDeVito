4218a8f10184ce41b9638a6bb594f1fda70cc129
amdevito <amdevitoam@gmail.com>
2020-11-29 21:32:20 -0500

    P2: Trying to set up records to play once pressed at certain points on the circles. Not working.

--- Projects/Project2/soundInstallation3.2_otherIdeas/js/script.js
+++ Projects/Project2/soundInstallation3.2_otherIdeas/js/script.js
@@ -9,6 +9,10 @@ let cirNum = 15;
 let cirSize = 10;
 let spiralType = 3; //randomize from  3-16
 let angle;
+let angle2;
+let angle3;
+let angle4;
+let angle5;
 
 let tracksPlayback;
 let files = [];
@@ -16,13 +20,12 @@ let files = [];
 let recordOne = {
   x: undefined,
   y: undefined,
-  directionF: undefined,
-  directionB: undefined,
   spin: 1,
   speed: 0.05,
   size: undefined,
   style: undefined,
-  active: false,
+  activeF: false,
+  activeB: false,
 };
 
 let recordTwo = {
@@ -30,11 +33,10 @@ let recordTwo = {
   y: undefined,
   spin: 1,
   speed: 0.05,
-  directionF: undefined,
-  directionB: undefined,
   size: undefined,
   style: undefined,
-  active: false,
+  activeF: false,
+  activeB: false,
 };
 
 let recordThree = {
@@ -42,11 +44,10 @@ let recordThree = {
   y: undefined,
   spin: 1,
   speed: 0.05,
-  directionF: undefined,
-  directionB: undefined,
   size: undefined,
   style: undefined,
-  active: false,
+  activeF: false,
+  activeB: false,
 };
 
 let recordFour = {
@@ -54,11 +55,10 @@ let recordFour = {
   y: undefined,
   spin: 1,
   speed: 0.05,
-  directionF: undefined,
-  directionB: undefined,
   size: undefined,
   style: undefined,
-  active: false,
+  activeF: false,
+  activeB: false,
 };
 
 let recordFive = {
@@ -66,11 +66,10 @@ let recordFive = {
   y: undefined,
   spin: 1,
   speed: 0.05,
-  directionF: undefined,
-  directionB: undefined,
   size: undefined,
   style: undefined,
-  active: false,
+  activeF: false,
+  activeB: false,
 };
 
 function preload() {
@@ -104,13 +103,15 @@ function draw() {
   //call function for the first record
   recordOneDisplay();
   //call function for the second record
-  // recordTwoDisplay();
+  recordTwoDisplay();
   // //call function for the third record
-  // recordThreeDisplay();
+  recordThreeDisplay();
   // //call function for the four record
-  // recordFourDisplay();
+  recordFourDisplay();
   // //call function for the five record
-  // recordFiveDisplay();
+  recordFiveDisplay();
+
+  checkMouseHover();
 
   // tracksPlayback.mousePosition();
 
@@ -162,39 +163,59 @@ function recordOneDisplay() {
     arc(0, 0, cirSize + gap * i, cirSize + gap * i, 45, 360 - i - angle);
     //cirSize = 0 makes it wobble
   }
-  // recordOneSpin();
+
+  //cirSize + gap * cirNum = 45?
 
   pop();
 }
 
-function recordOneSpin() {
+function recordOneSpinForward() {
   recordOne.spin = recordOne.spin + recordOne.speed;
+}
+function recordOneSpinBackward() {
+  recordOne.spin = recordOne.spin - recordOne.speed;
 }
 
 function recordTwoDisplay() {
   //second circle #2
   push();
   translate(recordTwo.x, recordTwo.y); //change to recordTwo.x, recordTWo.y
-  rotate(angle);
-  // angle++;
-  // angle = angle + map(mouseX, 0, width, -0.1, 0.1);
+  rotate(angle2);
+
+  angle2 = recordTwo.spin;
 
   noFill();
   stroke(255);
   strokeWeight(1);
 
   for (let i = 0; i < cirNum; i++) {
-    arc(0, 0, cirSize + gap * i, cirSize + gap * i, angle * i, 360 - angle / 2);
+    arc(
+      0,
+      0,
+      cirSize + gap * i,
+      cirSize + gap * i,
+      angle2 * i,
+      360 - angle2 / 2
+    );
   }
   pop();
+
+  // recordTwoSpin();
+}
+
+function recordTwoSpinForward() {
+  recordTwo.spin = recordTwo.spin + recordTwo.speed;
+}
+function recordTwoSpinBackward() {
+  recordTwo.spin = recordTwo.spin - recordTwo.speed;
 }
 
 function recordThreeDisplay() {
   //centre record #3
   push();
   translate(recordThree.x, recordThree.y);
-  rotate(angle);
-  angle = map(mouseX, 0, width, -90, 90);
+  rotate(angle3);
+  angle3 = recordThree.spin;
 
   noFill();
   stroke(255);
@@ -211,98 +232,194 @@ function recordThreeDisplay() {
     );
   }
   pop();
+
+  // recordThreeSpin();
+}
+
+function recordThreeSpinForward() {
+  recordThree.spin = recordThree.spin + recordThree.speed;
+}
+function recordThreeSpinBackward() {
+  recordThree.spin = recordThree.spin - recordThree.speed;
 }
 
 function recordFourDisplay() {
   //second last circle #4
   push();
   translate(recordFour.x, recordFour.y);
-  rotate(angle);
-  angle = angle + map(mouseX, 0, width, -0.1, 0.1);
+  rotate(angle4);
+  angle4 = recordFour.spin;
 
   noFill();
   stroke(255);
   strokeWeight(1);
 
   for (let i = 0; i < cirNum; i++) {
-    arc(0, 0, cirSize + gap * i, cirSize + gap * i, angle * i, 360 - angle / 2);
+    arc(
+      0,
+      0,
+      cirSize + gap * i,
+      cirSize + gap * i,
+      angle4 * i,
+      360 - angle4 / 2
+    );
   }
   pop();
+  // recordFourSpin();
+}
+
+function recordFourSpinForward() {
+  recordFour.spin = recordFour.spin + recordFour.speed;
+}
+function recordFourSpinBackward() {
+  recordFour.spin = recordFour.spin - recordFour.speed;
 }
 
 function recordFiveDisplay() {
   //last circle #5
   push();
   translate(recordFive.x, recordFive.y);
-  rotate(angle);
+  rotate(angle5);
   // angle = map(mouseX, 0, width, -90, 90);
-  angle = angle + map(mouseX, 0, width, -0.5, 0.5);
+  angle5 = recordFive.spin;
+
   noFill();
   stroke(255);
   strokeWeight(1);
 
   for (let i = 0; i < cirNum; i++) {
-    arc(0, 0, cirSize + gap * i, cirSize + gap * i, 45, 360 - i - angle);
+    arc(0, 0, cirSize + gap * i, cirSize + gap * i, 45, 360 - i - angle5);
   }
 
   pop();
+  // recordFiveSpin();
 }
 
-///click on a character to engage in a dialogue
+function recordFiveSpinForward() {
+  recordFive.spin = recordFive.spin + recordFive.speed;
+}
+function recordFiveSpinBackward() {
+  recordFive.spin = recordFive.spin - recordFive.speed;
+}
+
+///check if mouse is on top of the record, if on top and to left of centre, recordFive.spin = recordFive.spin - recordFive - recordFive.speed
 function checkMouseHover() {
-  let d1 = dist(mouseX, mouseY, personLongHair.x, personLongHair.y);
-  if (d1 < 20) {
-    personLongHair.active = true;
-  } else {
-    personLongHair.active = false;
+  let d1 = dist(mouseX, mouseY, recordOne.x, recordOne.y);
+  if (d1 < 20 && mouseX < recordOne.x) {
+    recordOne.activeB = true;
+  } else if (d1 < 20 && mouseX > recordOne.x) {
+    recordOne.activeF = true;
+  } else if (d1 < 20 && mouseX < recordOne.x && recordOne.activeB) {
+    recordOne.activeB = false;
+  } else if (d1 < 20 && mouseX > recordOne.x && recordOne.activeF) {
+    recordOne.activeF = false;
   }
-  let d2 = dist(mouseX, mouseY, personShortHair.x, personShortHair.y);
-  if (d2 < 20) {
-    personShortHair.active = true;
-  } else {
-    personShortHair.active = false;
+  let d2 = dist(mouseX, mouseY, recordTwo.x, recordTwo.y);
+  if (d2 < 22 && mouseX < recordTwo.x) {
+    recordTwo.activeB = true;
+  } else if (d2 < 22 && mouseX > recordTwo.x) {
+    recordTwo.activeF = true;
+  } else if (d2 < 22 && mouseX < recordTwo.x && recordTwo.activeB) {
+    recordTwo.activeB = false;
+  } else if (d2 < 22 && mouseX > recordTwo.x && recordTwo.activeF) {
+    recordTwo.activeF = false;
   }
-  let d3 = dist(mouseX, mouseY, childParent.x, childParent.y);
-  if (d3 < 20) {
-    childParent.active = true;
-  } else {
-    childParent.active = false;
+  let d3 = dist(mouseX, mouseY, recordThree.x, recordThree.y);
+  if (d3 < 22 && mouseX < recordThree.x) {
+    recordThree.activeB = true;
+  } else if (d3 < 22 && mouseX > recordThree.x) {
+    recordThree.activeF = true;
+  } else if (d3 < 22 && mouseX < recordThree.x && recordThree.activeB) {
+    recordThree.activeB = false;
+  } else if (d3 < 22 && mouseX > recordThree.x && recordThree.activeF) {
+    recordThree.activeF = false;
   }
-  let d4 = dist(mouseX, mouseY, digiCanvas1.x, digiCanvas1.y);
-  if (d4 < 50) {
-    digiCanvas1.active = true;
-  } else {
-    digiCanvas1.active = false;
+  let d4 = dist(mouseX, mouseY, recordFour.x, recordFour.y);
+  if (d4 < 22 && mouseX < recordFour.x) {
+    recordFour.activeB = true;
+  } else if (d4 < 22 && mouseX > recordFour.x) {
+    recordFour.activeF = true;
+  } else if (d4 < 22 && mouseX < recordFour.x && recordFour.activeB) {
+    recordFive.activeB = false;
+  } else if (d4 < 22 && mouseX > recordFour.x && recordFour.activeF) {
+    recordFour.activeF = false;
   }
-  let d5 = dist(mouseX, mouseY, digiCanvas2.x, digiCanvas2.y);
-  if (d5 < 50) {
-    digiCanvas2.active = true;
-  } else {
-    digiCanvas2.active = false;
+  let d5 = dist(mouseX, mouseY, recordFive.x, recordFive.y);
+  if (d5 < 22 && mouseX < recordFive.x && recordFive.activeB === false) {
+    recordFive.activeB = true;
+  } else if (d5 < 22 && mouseX > recordFive.x && recordFive.activeF === false) {
+    recordFive.activeF = true;
+  } else if (d5 < 22 && mouseX < recordFive.x && recordFive.activeB) {
+    recordFive.activeB = false;
+  } else if (d5 < 22 && mouseX > recordFive.x && recordFive.activeF) {
+    recordFive.activeF = false;
   }
-  // console.log(personLongHair.x);
-  // console.log(personLongHair.y);
-  // console.log(mouseX);
-  // console.log(mouseY);
-  // console.log(d1);
 }
 
 function mousePressed() {
+  //record one - press mouse play record forward or backwards or stop playing.
   //check if mouse is pressed while hovering over RECORD - change to play record when pressed. play forward if pressed to the right of center and play back wards if pressed to the left.
-  if (digiCanvas1.active && mousePressed) {
-    soundInstallation1(); //links to sonic Space invader
+  if (recordOne.activeF && mousePressed) {
+    recordOneSpinForward(); //play track one forward and spin record forward
+  } else if (!recordOne.activeF && mousePressed) {
+    return;
   }
-  if (digiCanvas2.active && mousePressed) {
-    soundInstallation2(); //links to kelidoscape
+
+  if (recordOne.activeB && mousePressed) {
+    recordOneSpinBackward();
+  } else if (!recordOne.activeB && mousePressed) {
+    return;
   }
-  if (personLongHair.active && mousePressed) {
-    narrative1(); //links to sonic Space invader
+  //record two - press mouse play record forward or backwards or stop playing.
+  if (recordTwo.activeF && mousePressed) {
+    recordTwoSpinForward(); //play track one forward and spin record forward
+  } else if (!recordTwo.activeF && mousePressed) {
+    return;
   }
-  if (personShortHair.active && mousePressed) {
-    narrative2(); //links to kelidoscape
+
+  if (recordTwo.activeB && mousePressed) {
+    recordTwoSpinBackward();
+  } else if (!recordTwo.activeB && mousePressed) {
+    return;
   }
-  if (childParent.active && mousePressed) {
-    narrative3(); //links to sonic Space invader - for now, will be new generative virtual sound installation piece
+
+  //record three - press mouse play record forward or backwards or stop playing.
+  if (recordThree.activeF && mousePressed) {
+    recordThreeSpinForward(); //play track one forward and spin record forward
+  } else if (!recordThree.activeF && mousePressed) {
+    return;
+  }
+
+  if (recordThree.activeB && mousePressed) {
+    recordThreeSpinBackward();
+  } else if (!recordThree.activeB && mousePressed) {
+    return;
+  }
+
+  //record four - press mouse play record forward or backwards or stop playing.
+  if (recordFour.activeF && mousePressed) {
+    recordFourSpinForward(); //play track one forward and spin record forward
+  } else if (!recordFour.activeF && mousePressed) {
+    return;
+  }
+
+  if (recordFour.activeB && mousePressed) {
+    recordFourSpinBackward();
+  } else if (!recordFour.activeB && mousePressed) {
+    return;
+  }
+
+  //record five - press mouse play record forward or backwards or stop playing.
+  if (recordFive.activeF && mousePressed) {
+    recordFiveSpinForward(); //play track one forward and spin record forward
+  } else if (!recordFive.activeF && mousePressed) {
+    return;
+  }
+
+  if (recordFive.activeB && mousePressed) {
+    recordFiveSpinBackward();
+  } else if (!recordFive.activeB && mousePressed) {
+    return;
   }
 }
 
@@ -310,12 +427,13 @@ function mousePressed() {
 //if active and mouseX is descreasing to the left of centre and mousedragged then play backward
 //if active and mouseY is descreasing (going up the screen) play faster +1 as you get closer to 0
 //if active and mouseY is increasing (going down) play slower -1 as you get closer to height.
-//call this code for mouse dragged
-function mouseDragged() {
-  value = value + 5;
 
-  if (value > 255) {
-    value = 0;
-  }
-}
+//call this code for mouse dragged
+// function mouseDragged() {
+//   value = value + 5;
+//
+//   if (value > 255) {
+//     value = 0;
+//   }
+// }
 //code inspired by https://www.youtube.com/watch?v=QjCtbMNLRuc
